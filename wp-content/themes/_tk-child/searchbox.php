6a13a013f80752bf6d32bdbcd2cdf80ea737acf6
David Helberg <helberg.david@gmail.com>
2017-07-15 14:50:12 +0000

    added search box and updated home.php

---
+++ wp-content/themes/_tk-child/searchbox.php
@@ -0,0 +1,12 @@
+<div id="searchBox">
+  <div class="row">
+    <div class="col-lg-6 col-lg-offset-3">
+      <div class="input-group">
+        <input type="text" class="form-control" placeholder="Search for...">
+        <span class="input-group-btn">
+          <button class="btn btn-secondary" type="button">Go!</button>
+        </span>
+      </div>
+    </div>
+  </div>
+</div>
