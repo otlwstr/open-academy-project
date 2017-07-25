2285b09ff019b27a969a7d18dc9191f3846b6ed5
David Helberg <helberg.david@gmail.com>
2017-07-25 23:26:25 +0000

    fixed nav bar

--- wp-content/themes/_tk-child/search.php
+++ wp-content/themes/_tk-child/search.php
@@ -1,26 +1,4 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-        <title> </title>
-        <?php wp_head(); ?>
-    </head>
-    <body>
-        <!--navigation-->
-        <div class="navbar navbar-inverse navbar-fixed-left">
-          <a class="navbar-brand" href="#"><i class="fa fa-graduation-cap" aria-hidden="true"></i></a>
-          <ul class="nav navbar-nav">
-           <?php get_search_form(); ?>
-           <li><a href="#home"><i class="fa fa-home"></i><span> Link1 </span></a></li>
-           <li><a href="#info"><i class="fa fa-info-circle"></i><span> Link2 </span></a></li>
-           <li><a href="#love"><i class="fa fa-heart"></i><span> Link3 </span></a></li>
-           <li><a href="#work"><i class="fa fa-briefcase"></i><span> Link4 </span></a></li>
-           <li><a href="#contact"><i class="fa fa-envelope"></i><span> Link5 </span></a></li>
-          </ul>
-        </div>
-        <!--end navigation-->
-        <!-- 1st section-->
-            <div class="wrapper">
+            <?php get_header(); ?>
                 <div class="container" id="home">
                     <div class="row">
                        <h2>Link 1</h2>
@@ -52,6 +30,4 @@
             );
         ?>
         </div> </div> </nav>
-        <?php wp_footer(); ?>
-    </body>
-</html>
+        <?php get_footer(); ?>
