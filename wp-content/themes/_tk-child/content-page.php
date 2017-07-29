9c983f3c181b07561b96fd2d19f033ecc80a8297
Marlon <marlon.taylor83@gmail.com>
2017-07-29 14:08:05 +0000

    update content and pages

--- wp-content/themes/_tk-child/content-page.php
+++ wp-content/themes/_tk-child/content-page.php
@@ -23,6 +23,7 @@ get_header(); ?>
     ?>
     <?php while ( $loop->have_posts() ) : $loop->the_post();?>
         <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <div class="entry-content"><?php the_title(); ?></div>
             <div class="entry-content"><?php the_content(); ?></div>
         </article>
  
