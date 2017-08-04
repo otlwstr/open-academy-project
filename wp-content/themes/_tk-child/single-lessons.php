fd79462b4977d21d2c333b35ec6cc880aafb6d03
David Helberg <helberg.david@gmail.com>
2017-08-04 01:03:24 +0000

    pusing

--- wp-content/themes/_tk-child/single-lessons.php
+++ wp-content/themes/_tk-child/single-lessons.php
@@ -17,46 +17,19 @@ get_header(); ?>
     
     <?php
     
-    $lessonspost = array( 'post_type' => 'lesson', );
-    $loop = new WP_Query( $lessonspost );
-        //print_r($loop);die();
-    
+    $coursespost = array( 'post_type' => 'lessons', );
+    $loop = new WP_Query( $coursespost );
+
+
     ?>
-    <?php while ( $loop->have_posts() ) : $loop->the_post();?>
+
         <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
             <div class="entry-content"><?php the_content(); ?></div>
         </article>
- 
-    <?php endwhile; ?>
+
     
     </div>
     
 </div>
-         <ul>test
-<?php
-
-    $currentTaxonomy = get_queried_object_id();
-
-    $args = array('post_type' => 'lessons',
-        'tax_query' => array(
-            array(
-                'taxonomy' => 'course',
-                'field' => 'id',
-                'terms' => $currentTaxonomy,
-            ),
-        ),
-     );
-
-     $loop = new WP_Query($args);
-    //print_r($loop); die();
-     if($loop->have_posts()) {
-        echo '<h2>'.$custom_term->name.'</h2>';
-
-        while($loop->have_posts()) : $loop->the_post();
-            echo '<li><a href="'.get_permalink().'">'.get_the_title().'</a></li>';
-        endwhile;
-     }
-     
-?>     
-</ul>
+ 
 <?php get_footer(); ?>
